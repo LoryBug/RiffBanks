9ad1f849f82cb228033305a4da78ba933f8ce317
LoryBug <72506393+LoryBug@users.noreply.github.com>
2025-11-29 09:46:15 +0100

    add new song routes with assets

--- server/routes/songs.js
+++ server/routes/songs.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const songController = require('../controllers/songController');
 
 router.get('/', songController.list);
+router.post('/', songController.create);
 router.get('/:id', songController.get);
+router.patch('/:id', songController.update);
 router.delete('/:id', songController.delete);
 
 module.exports = router;
