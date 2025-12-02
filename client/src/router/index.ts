a622824ab5c94070cfd2a51933b07981d372eccf
LoryBug <72506393+LoryBug@users.noreply.github.com>
2025-12-02 16:09:55 +0100

    Add bandInfoview

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -32,24 +32,24 @@ const routes = [
     component: () => import('@/views/BandInfoView.vue'),
     meta: { requiresAuth: true, requiresOnboarding: true }
   },
-  {
-    path: '/band/:bandId/songs',
-    name: 'songs',
-    component: () => import('@/views/SongListView.vue'),
-    meta: { requiresAuth: true, requiresOnboarding: true }
-  },
-  {
-    path: '/band/:bandId/songs/new',
-    name: 'create-song',
-    component: () => import('@/views/CreateSongView.vue'),
-    meta: { requiresAuth: true, requiresOnboarding: true }
-  },
-  {
-    path: '/song/:id',
-    name: 'song-detail',
-    component: () => import('@/views/SongDetailView.vue'),
-    meta: { requiresAuth: true, requiresOnboarding: true }
-  },
+  // {
+  //   path: '/band/:bandId/songs',
+  //   name: 'songs',
+  //   component: () => import('@/views/SongListView.vue'),
+  //   meta: { requiresAuth: true, requiresOnboarding: true }
+  // },
+  // {
+  //   path: '/band/:bandId/songs/new',
+  //   name: 'create-song',
+  //   component: () => import('@/views/CreateSongView.vue'),
+  //   meta: { requiresAuth: true, requiresOnboarding: true }
+  // },
+  // {
+  //   path: '/song/:id',
+  //   name: 'song-detail',
+  //   component: () => import('@/views/SongDetailView.vue'),
+  //   meta: { requiresAuth: true, requiresOnboarding: true }
+  // },
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
