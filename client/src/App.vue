1cc952a842293eede173585c637c1bf7a38b75ac
LoryBug <72506393+LoryBug@users.noreply.github.com>
2025-12-03 15:58:38 +0100

    add socket to app

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,15 +3,29 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, watch } from 'vue'
 import { useAuthStore } from '@/stores/auth'
+import { useSocketStore } from '@/stores/socket'
 
 const authStore = useAuthStore()
+const socketStore = useSocketStore()
 
 onMounted(async () => {
   await authStore.checkAuth()
+  if (authStore.isAuthenticated) {
+    socketStore.connect()
+  }
 })
 
-
+watch(
+  () => authStore.isAuthenticated,
+  (isAuthenticated: any) => {
+    if (isAuthenticated) {
+      socketStore.connect()
+    } else {
+      socketStore.disconnect()
+    }
+  }
+)
 </script>
 <style scoped></style>
